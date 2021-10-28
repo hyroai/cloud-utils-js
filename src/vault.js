7ac1ac7c0b931edfcb32ba8a35cbda307518b4b8
erezhaim <erezh.work@gmail.com>
2021-10-27 23:09:48 -0500

    Adding resultPath helper function

--- src/vault.js
+++ src/vault.js
@@ -4,21 +4,19 @@ const { asyncPipe, withCacheAsync } = require("gamlajs").default;
 
 const baseMetadataUrl = "http://169.254.169.254/metadata";
 
-const identityToken = asyncPipe(
-  () =>
-    axios.get(
-      `${baseMetadataUrl}/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F`,
-      { headers: { Metadata: "true" } }
-    ),
-  path(["data", "access_token"])
+const getResultPath = (url, headers, resultPath) =>
+  asyncPipe(() => axios.get(url, { headers }), path(resultPath));
+
+const identityToken = getResultPath(
+  `${baseMetadataUrl}/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F`,
+  { Metadata: "true" },
+  ["data", "access_token"]
 );
 
-const instanceMetadata = asyncPipe(
-  () =>
-    axios.get(`${baseMetadataUrl}/instance?api-version=2019-08-15`, {
-      headers: { Metadata: "true" },
-    }),
-  path(["data"])
+const instanceMetadata = getResultPath(
+  `${baseMetadataUrl}/instance?api-version=2019-08-15`,
+  { Metadata: "true" },
+  ["data"]
 );
 
 const vaultAuthPayload = curry(
@@ -50,11 +48,11 @@ const vaultHeaders = (tokenGetter) => async () => ({
 });
 
 const readKey = ({ baseVaultUrl, headersGetter }) => async (path) =>
-  (
-    await axios.get(`${baseVaultUrl}/secret/data/${path}`, {
-      headers: await headersGetter(),
-    })
-  ).data.data;
+  getResultPath(`${baseVaultUrl}/secret/data/${path}`, await headersGetter(), [
+    "data",
+    "data",
+    "data",
+  ])();
 
 const writeKey = ({ baseVaultUrl, headersGetter }) => async (path, value) =>
   axios.post(
