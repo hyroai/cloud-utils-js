a460be8476408e7caea0ba7a70df85f75b11e818
Liran Bakalash <liran.b@hyro.ai>
2023-08-22 08:35:19 +0300

    run pre-commit

--- src/cache/redis/__mocks__/RedisClient.js
+++ src/cache/redis/__mocks__/RedisClient.js
@@ -1,7 +1,7 @@
 const redisMock = require("redis-mock");
 const { cacheApiResources } = require("../utils");
 
-const redisClient = redisMock.createClient()
+const redisClient = redisMock.createClient();
 
 module.exports = () => ({
   redisClient,
